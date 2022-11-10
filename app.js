1c6c38406faac398367e15d6484b6bab6ac4b57b
Rebonace <celiarebonato@gmail.com>
2022-11-10 10:07:15 +0100

    added post route in userHandlers file

--- app.js
+++ app.js
@@ -16,11 +16,13 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
 
 const usersHandlers = require("./usersHandlers");
 
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUserById);
+app.post("/api/users", usersHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
